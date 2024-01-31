88dd7edad0f7b718724f503dbf092616fe47589e
Betsy <132311670+betsymao@users.noreply.github.com>
2024-01-31 14:56:37 +1100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+// libraries
+import { 
+  Routes, 
+  Route 
+} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import Error from './pages/Error';
+
+// components
+import Layout from './components/layout/Layout';
+
+// styles
+import './App.css';
+
 function App() {
   return (
     <>
+      <Routes>
+
+        {/* layout component */}
+        <Route path='/' element={<Layout />}>
+
+        {/* home page */}
+        <Route index element={<Home />} />
+
+        {/* error page */}
+        <Route path='*' element={<Error />} />
+
+        </Route>
+
+      </Routes>
     </>
   )
 };
