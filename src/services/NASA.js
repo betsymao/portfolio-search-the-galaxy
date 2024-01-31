3ab015c223774ec3ad59dbf6c5403964ce3a48be
Betsy <132311670+betsymao@users.noreply.github.com>
2024-01-31 14:55:02 +1100

    updated services

---
+++ src/services/NASA.js
@@ -0,0 +1,13 @@
+// libraries
+import axios from 'axios';
+
+// config
+import config from '../config/config.json';
+
+// connect to API
+export function getItems(query) {
+
+    // GET request
+    return axios.get(`${config.NASARootURL}search?q=${query}&page_size=20`);
+
+}
